852d26d8357da52bfc78d8437bb9058027d58cc9
然 张 <zhangranmm@126.com>
2019-12-04 18:42:30 +0800

    88

--- Base/Notifier.cs
+++ Base/Notifier.cs
@@ -11,6 +11,12 @@ namespace WPF_StatusNotification.Base
 {
     public class Notifier:NotifierViewBase
     {
+        static string infoURI = "/WPF_StatusNotification;component/Resource/info.png";
+        static string errorURI = "/WPF_StatusNotification;component/Resource/error.png";
+        static string successURI = "/WPF_StatusNotification;component/Resource/success.png";
+        static string warnURI = "/WPF_StatusNotification;component/Resource/warn.png";
+
+
         public static void ShowView(NotifierViewBase view, ShowOptions options = null)
         {
             if (view == null)
@@ -35,13 +41,13 @@ namespace WPF_StatusNotification.Base
             view.Show();
         }
 
-        public static void ShowInfo(string header,string content,bool isAutoClose=true,int showTimeIfAutoCloseMS=5000)
+        internal static void Show(string header,string content,string ImageUri, bool isAutoClose=true,int showTimeIfAutoCloseMS=5000)
         {
             NotifierViewModel viewModel = new NotifierViewModel()
             {
                 Header = header,
                 Context = content,
-                ImageSource = new BitmapImage(new Uri("/WPF_StatusNotification;component/Resource/info.png", UriKind.Relative))
+                ImageSource = new BitmapImage(new Uri(ImageUri, UriKind.Relative))
         };
             ShowOptions showOptions = new ShowOptions()
             {
@@ -53,5 +59,27 @@ namespace WPF_StatusNotification.Base
             view.SizeToContent = System.Windows.SizeToContent.WidthAndHeight;
             Notifier.ShowView(view, showOptions);
         }
+
+        public static void ShowInfo(string header, string content, bool isAutoClose = true, int showTimeIfAutoCloseMS = 5000)
+        {
+            Show(header, content, infoURI, isAutoClose, showTimeIfAutoCloseMS);
+        }
+
+        public static void ShowError(string header, string content, bool isAutoClose = true, int showTimeIfAutoCloseMS = 5000)
+        {
+            Show(header, content, errorURI, isAutoClose, showTimeIfAutoCloseMS);
+        }
+
+        public static void ShowSuccess(string header, string content, bool isAutoClose = true, int showTimeIfAutoCloseMS = 5000)
+        {
+            Show(header, content, successURI, isAutoClose, showTimeIfAutoCloseMS);
+        }
+
+        public static void ShowWarn(string header, string content, bool isAutoClose = true, int showTimeIfAutoCloseMS = 5000)
+        {
+            Show(header, content, warnURI, isAutoClose, showTimeIfAutoCloseMS);
+        }
+
+
     }
 }
